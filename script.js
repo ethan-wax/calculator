576db7e19d672871432a1e3ee0d3a6010e38df1f
Ethan Waxman <ejw99@cornell.edu>
2020-06-03 02:21:04 -0400

    Number buttons working

--- script.js
+++ script.js
@@ -29,3 +29,52 @@ function operate(a, b, op){
         divide(a,b);
     }
 };
+
+const display = document.querySelector('#display');
+
+var running = '';
+function append(a){
+    running = running + a;
+    display.textContent = running;
+}
+
+const btn0 = document.querySelector('#btn0');
+btn0.addEventListener('click', (e) => {
+    append(0);
+});
+const btn1 = document.querySelector('#btn1');
+btn1.addEventListener('click', (e) => {
+    append(1);
+});
+const btn2 = document.querySelector('#btn2');
+btn2.addEventListener('click', (e) => {
+    append(2);
+});
+const btn3 = document.querySelector('#btn3');
+btn3.addEventListener('click', (e) => {
+    append(3);
+});
+const btn4 = document.querySelector('#btn4');
+btn4.addEventListener('click', (e) => {
+    append(4);
+});
+const btn5 = document.querySelector('#btn5');
+btn5.addEventListener('click', (e) => {
+    append(5);
+});
+const btn6 = document.querySelector('#btn6');
+btn6.addEventListener('click', (e) => {
+    append(6);
+});
+const btn7 = document.querySelector('#btn7');
+btn7.addEventListener('click', (e) => {
+    append(7);
+});
+const btn8 = document.querySelector('#btn8');
+btn8.addEventListener('click', (e) => {
+    append(8);
+});
+const btn9 = document.querySelector('#btn9');
+btn9.addEventListener('click', (e) => {
+    append(9);
+});
